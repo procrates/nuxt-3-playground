06db7fcd8e7f29f7c25b2aa2cdbc78444afb2c5f
Mikael Martinse <mikmar168@gmail.com>
2022-01-03 22:17:26 +0100

    netlify form from gridosme tut

--- pages/netlify-form/index.vue
+++ pages/netlify-form/index.vue
@@ -1,20 +1,62 @@
 <template>
-    <form name="contact" data-netlify="true">
-        <p>netlify test form</p>
-        <p>
+    <form
+        name="contact"
+        method="post"
+        v-on:submit.prevent="handleSubmit"
+        action="/success/"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+    >
+        <input type="hidden" name="form-name" value="contact" />
+        <p hidden>
             <label>
-                Name
-                <input type="text" name="name" />
+                Don’t fill this out:
+                <input name="bot-field" />
             </label>
         </p>
-        <p>
-            <label>
-                Email
-                <input type="email" name="email" />
-            </label>
-        </p>
-        <p>
-            <button type="submit">Send</button>
-        </p>
+        <div class="sender-info">
+            <div>
+                <label for="name" class="label">Your name</label>
+                <input type="text" name="name" v-model="formData.name" />
+            </div>
+            <div>
+                <label for="email">Your email</label>
+                <input type="email" name="email" v-model="formData.email" />
+            </div>
+        </div>
+
+        <div class="message-wrapper">
+            <label for="message">Message</label>
+            <textarea name="message" v-model="formData.message"></textarea>
+        </div>
+
+        <button type="submit">Submit form</button>
     </form>
 </template>
+
+<script setup lang="ts">
+const formData = reactive({
+    message: "",
+    email: "",
+    name: ""
+})
+const router = useRouter()
+
+const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+        .join('&')
+}
+const handleSubmit = (e) => {
+    fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: encode({
+            'form-name': e.target.getAttribute('name'),
+            ...formData,
+        }),
+    })
+        .then(() => router.push('/netlify-form/success'))
+        .catch(error => alert(error))
+}
+</script>
